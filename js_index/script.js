d176d1c34ea11c096cb184f8869318d5ec211e27
Doniyorbek <142497625+DevteachCoder@users.noreply.github.com>
2026-02-24 14:41:35 +0500

    Add files via upload

---
+++ js_index/script.js
@@ -0,0 +1,18 @@
+// function myFunction() {
+//     let el = document.getElementById("newDiv");
+//     el.classList.add("myStyle");
+// }
+
+// let se = false;
+// document.querySelector('button').addEventListener('click',(e) => {
+//     const el = e.target
+//     se = !se;
+//     if(se) {
+//         el.innerText = "now secure";
+//         el.classList.add("Secure")
+//     }else {
+//         el.innerText = "Click for secure";
+//         el.classList.remove("Secure")
+//         el.classList.add("SecureTwo")
+//     }
+// })
